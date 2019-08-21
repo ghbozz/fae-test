532116c951edbb27dae300690240d972e7d1d7bb
romain sanson <romainsanson.gm@gmail.com>
2019-08-21 20:56:30 +0200

    fae cms test

---
+++ app/models/article_category.rb
@@ -0,0 +1,13 @@
+class ArticleCategory < ApplicationRecord
+  include Fae::BaseModelConcern
+
+  acts_as_list add_new_at: :top
+  default_scope { order(:position) }
+
+  def fae_display_field
+    name
+  end
+
+  has_many :articles
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  resources :articles, only: [:index, :show]
+
+  namespace :admin do
+    resources :articles
+    resources :article_categories
+  end
+
+  # mount Fae below your admin namespec
+  mount Fae::Engine => '/admin'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
